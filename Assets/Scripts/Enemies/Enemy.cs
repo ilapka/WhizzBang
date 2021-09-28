57ceea23281eb39c536d788e82849c5a10a53fd9
ivan <ilapk@mail.ru>
2021-09-28 20:46:19 +0300

    Add enemies

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using WhizzBang.Data;
+using WhizzBang.Interfaces;
+using WhizzBang.Spawners;
+
+namespace WhizzBang.Enemies
+{
+    public class Enemy : SpawnableObject, IHaveHealth
+    {
+        [SerializeField] private EnemyData enemyData;
+        public float Health { get; private set; }
+
+        private void Start()
+        {
+            Health = enemyData.health;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+            if (Health <= 0f)
+            {
+                Die();
+            }
+        }
+        
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
