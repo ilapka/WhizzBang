c93589ded8f1d7066a9bdea5bb50dca119434f05
ivan <ilapk@mail.ru>
2021-09-28 18:10:14 +0300

    Add inputs

---
+++ Assets/Scripts/Inputs/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WhizzBang.Inputs
+{
+    public class PlayerInput : InputShell
+    {
+        private void FixedUpdate()
+        {
+            UpdateAxisInput();
+        }
+
+        private void UpdateAxisInput()
+        {
+            updateAxisEvent.Invoke(new InputAxis()
+            {
+                horizontalInput = Input.GetAxis("Horizontal"),
+                verticalInput = Input.GetAxis("Vertical"),
+            });
+        }
+    }
+}
