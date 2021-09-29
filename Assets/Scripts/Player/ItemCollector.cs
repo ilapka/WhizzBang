4bfbc4b239139efb0b15e3bc88a5aa4829d896d0
ivan <ilapk@mail.ru>
2021-09-30 00:48:44 +0300

    Add radius value to ItemCollector

--- Assets/Scripts/Player/ItemCollector.cs
+++ Assets/Scripts/Player/ItemCollector.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using WhizzBang.Inventories;
 using WhizzBang.Inventories.PickUpItems;
 
 namespace WhizzBang.Player
 {
-    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(CapsuleCollider))]
     public class ItemCollector : MonoBehaviour
     {
         [SerializeField] private Inventory _playerInventory;
+        [SerializeField] private CapsuleCollider _capsuleCollider;
+        [SerializeField] private float _collectionRadius;
+
+        private void Start()
+        {
+            _capsuleCollider.radius = _collectionRadius;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
